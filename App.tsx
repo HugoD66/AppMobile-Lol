08bbdc0a70edbcb4a9131ed3a46fc51f054a0c49
HugoD66 <dessauw.hugo@gmail.com>
2023-11-07 14:28:18 +0100

    Init Project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View} from 'react-native';
+import FirstSlide from './app/pages/intro/FirstSlide';
+import SecondSlide from "./app/pages/intro/SecondSlide";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <SecondSlide />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ app/pages/intro/FourthSlide.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import {Alert, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+export default function FourthSlide() {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() => Alert.alert('Coucou')}>
+                <Image source={require('../../../assets/intro/introLogo.png')} />
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
