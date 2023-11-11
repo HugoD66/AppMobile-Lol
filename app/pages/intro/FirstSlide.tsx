056ccd446177e059e7e9638e2baaa200a49ec3f6
HugoD66 <dessauw.hugo@gmail.com>
2023-11-11 17:59:34 +0100

    Save projet, Template slide 4 ok

--- app/pages/intro/FirstSlide.tsx
+++ app/pages/intro/FirstSlide.tsx
@@ -38,6 +38,7 @@ export function FirstSlide() {
                     source={require('../../../assets/intro/introLogo.png')}
                 />
             </TouchableOpacity>
+
         </View>
     );
 }
