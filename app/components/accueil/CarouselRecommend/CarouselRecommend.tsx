57e13ac3198526f822e03908ac12442b4c7c1dda
HugoD66 <dessauw.hugo@gmail.com>
2023-11-14 14:40:44 +0100

    Add Carousel Vi, before Dorian Pull

--- app/components/accueil/CarouselRecommend/CarouselRecommend.tsx
+++ app/components/accueil/CarouselRecommend/CarouselRecommend.tsx
@@ -1,24 +1,39 @@
-import {StyleSheet, View} from "react-native";
+import {ScrollView, StyleSheet, View} from "react-native";
 import {CardRecommend} from "./CardRecommend";
 
 export function CarouselRecommend() {
   return (
-    <View style={styles.container}>
-      <CardRecommend />
-      <CardRecommend />
-    </View>
+      <ScrollView horizontal contentContainerStyle={styles.container}>
+      <View style={styles.cards}>
+        <CardRecommend />
+        <CardRecommend />
+        <CardRecommend />
+        <CardRecommend />
+
+      </View>
+      </ScrollView>
 
   )
 }
 const styles = StyleSheet.create({
   container: {
-
-    width: '100%',
-    backgroundColor: 'orange',
+    paddingVertical: 0,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
     display: 'flex',
     flexDirection: 'row',
-    height: 250,
+    height: 300,
+
+  },
+  cards: {
+    display: 'flex',
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    overflow: 'scroll',
+    paddingHorizontal: 0,
+    height: 280,
   },
 });
