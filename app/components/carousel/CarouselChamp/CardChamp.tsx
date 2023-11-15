57a45ae4c483b6f0b89174190a6d023c7640e30a
Dessauw Hugo <dessauw.hugo@gmail.com>
2023-11-15 20:15:20 +0100

    Continue search screen

--- app/components/carousel/CarouselChamp/CardChamp.tsx
+++ app/components/carousel/CarouselChamp/CardChamp.tsx
@@ -1,15 +1,9 @@
 import {StyleSheet, Image, Text, TouchableOpacity} from "react-native";
 import {useNavigation} from "@react-navigation/native";
-<<<<<<< HEAD:app/components/carousel/CarouselChamp/CardChamp.tsx
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {StackParamList} from "../../../pages/accueil/Accueil";
 import { CardChampProps } from "../../../types/CardChampProps"
-=======
-import {StackParamList} from "../../../App";
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
->>>>>>> 913e399bedbb350a3f9042496f09048078ae7ea4:app/components/carousel/CardChamp.tsx
 
-export type StackNavigationProps = NativeStackNavigationProp<StackParamList, 'Accueil'>
 
 export type StackNavigationProps = NativeStackNavigationProp<StackParamList, 'Accueil'>
 
@@ -18,10 +12,6 @@ export function CardChamp({titleCard, subTitleCard, imageCard}: CardChampProps) 
 
   console.log({titleCard})
   const navigate = () => {
-<<<<<<< HEAD:app/components/carousel/CarouselChamp/CardChamp.tsx
-=======
-    console.log({navigation: navigation.getState()})
->>>>>>> 913e399bedbb350a3f9042496f09048078ae7ea4:app/components/carousel/CardChamp.tsx
     navigation.navigate('Champion', {
       nom: titleCard,
     });
--- app/pages/search/Search.tsx
+++ app/pages/search/Search.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import {  StyleSheet,  View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SCREEN_HEIGHT, SCREEN_WIDTH} from "../../types/screenDim";
+import {LinearGradient} from "expo-linear-gradient";
+import {BeforeSearch} from "./BeforeSearch";
+import {SearchBar} from "react-native-screens";
 
 
 export function Search() {
 
   return (
     <View style={styles.container}>
+      <BeforeSearch />
+      <View style={styles.contentSearch} >
+        <TouchableOpacity>
+          <Image style={styles.arrowBack} source={require('../../../assets/search/arrow-back.png')} />
+        </TouchableOpacity>
+        <View style={styles.searchbarContent}>
+          <View style={styles.searchbar}>
+            <TouchableOpacity>
+              <Image style={styles.loop} source={require('../../../assets/general/loopSumNav.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
 
-
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    flex: 1,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
     backgroundColor: 'black',
+  },
+
+  contentSearch: {
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SCREEN_HEIGHT * 0.33 ,
+  },
+  arrowBack: {
+
+  },
+  searchbarContent: {
+
+  },
+
+  searchbar: {
+    width: SCREEN_WIDTH * 0.9,
+    height: SCREEN_HEIGHT * 0.05,
+    backgroundColor: '#1E1F24',
+    borderRadius: 20,
+    zIndex: 3,
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loop: {
 
+  },
 });
