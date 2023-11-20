57f55d2b2a595481729a8aa2d8dde18733efde39
Dessauw Hugo <dessauw.hugo@gmail.com>
2023-11-20 20:14:31 +0100

    Login start page

--- app/pages/login/login.tsx
+++ app/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import {KeyboardAvoidingView, TextInput, StyleSheet, TouchableOpacity, Text, View, Image} from 'react-native';
+import {KeyboardAvoidingView, TextInput, StyleSheet, TouchableOpacity, Text, View, Image, Platform} from 'react-native';
 import React from 'react';
 import {SCREEN_HEIGHT, SCREEN_WIDTH} from "../../types/screenDim";
 
@@ -8,57 +8,56 @@ export function Login() {
       <View style={styles.containerBackground}>
         <Image style={styles.pictureback} source={require('../../../assets/login/pictureback-login.png')} />
       </View>
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <TextInput style={styles.textInput} placeholder="Tapez ici..." />
-        <TextInput style={styles.textInput} placeholder="Tapez ici..." />
-        <TextInput style={styles.textInput} placeholder="Tapez ici..." />
-        <TouchableOpacity>
-          <Text style={styles.buttonLogin}>Connexion</Text>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+      <View style={styles.contentLogin}>
+        <KeyboardAvoidingView    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                                 >
+          <TextInput style={styles.textInput} placeholder="Tapez ici..." />
+          <TextInput style={styles.textInput} placeholder="Tapez ici..." />
+          <TextInput style={styles.textInput} placeholder="Tapez ici..." />
+          <TouchableOpacity>
+            <Text style={styles.buttonLogin}>Connexion</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
     </View>
 
   );
-
 }
+/*
+
+ */
 const styles = StyleSheet.create({
-  containerBackground: {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-    position: 'absolute',
-    backgroundColor: '#050505',
-    alignItems: 'center',
-  },
-  pictureback: {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-    resizeMode: 'contain',
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: 'black',
-    zIndex: 3,
-  },
   container: {
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
+    backgroundColor: '#1f1f1f',
+  },
+  containerBackground: {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+  },
+  pictureback: {
+    width: '100%',
     position: 'absolute',
-    backgroundColor: 'white',
-    alignItems: 'center',
+  },
+  contentLogin: {
+    position: 'absolute',
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    display: 'flex',
     justifyContent: 'space-around',
+    alignItems: 'center',
+
   },
   textInput: {
-    backgroundColor: 'black',
     fontWeight: 'bold',
     fontSize: 20,
-    color: 'white',
+
+    backgroundColor: 'white',
+    zIndex: 4,
 
   },
   buttonLogin: {
     height: 80,
-    backgroundColor: 'black',
-    zIndex: 4,
   },
 });
